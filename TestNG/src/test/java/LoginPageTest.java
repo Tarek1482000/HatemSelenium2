cf28d10b19c5b664505042b4015399c2626922a0
Tarek Ahmed Abd El Wahab Shafei <115722766+Tarek1482000@users.noreply.github.com>
2024-08-26 12:35:47 +0300

    Add files via upload

---
+++ TestNG/src/test/java/LoginPageTest.java
@@ -0,0 +1,29 @@
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+import static org.testng.Assert.assertTrue;
+
+public class LoginPageTest extends TestBase {
+
+    @Test
+    public static void ValidLoginTC() {
+        loginPage.EnterEmail(email);
+        loginPage.EnterPassword(password);
+        loginPage.ClickingOnRememberMe();
+        // Get the driver of home page
+        coursePage = loginPage.ClickingOnLogin();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        Assert.assertTrue(coursePage.getSuccessLogin().getText().contains("Institute Management"),
+                "Login was not successful");
+    }
+
+    @AfterTest
+    public void CloseLogin()
+    {
+        driver.quit();
+    }
+}
